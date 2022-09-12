723c9a4ddcc1da91691616e6a19c3cd20dcc7c72
stego135 <beckerfiona135@gmail.com>
2022-09-12 10:27:06 -0400

    more tests

--- Bootcamp-API.Tests/PokemonTest.cs
+++ Bootcamp-API.Tests/PokemonTest.cs
@@ -1,5 +1,6 @@
 using Bootcamp_API.Controllers;
 using Bootcamp_API.Models;
+using Bootcamp_API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bootcamp_API.Tests;
@@ -43,5 +44,50 @@ public class PokemonTest
         var pokemon = result.Value;
 
         pokemon.Should().BeEquivalentTo(sampleData);
+    }
+
+    [Fact]
+    public void Get_With_Wrong_Id_Returns_Null()
+    {
+        var controller = new PokemonController();
+
+        var result = controller.Get(3);
+        var pokemon = result.Value;
+
+        Assert.Equal(null, pokemon);
+    }
+
+    [Fact]
+    public void Add_And_Delete_Increases_List_Correctly()
+    {
+        var controller = new PokemonController();
+        var newPokemon = new Pokemon {Id = 1, Name = "Mew", Count = 5025, UserId = 1};
+
+        var result = controller.Create(newPokemon);
+        var pokemonResult = controller.Get(3);
+        var pokemon = pokemonResult.Value;
+
+        pokemon.Should().BeEquivalentTo(newPokemon);
+
+        controller.Delete(3);
+        var listResult = controller.GetAll();
+        var list = listResult.Value;
+
+        Assert.Equal(2, list.Count);
+    }
+
+    [Fact]
+    public void Update_Updates_Pokemon_Correctly()
+    {
+        var controller = new PokemonController();
+        var updatedPokemon = new Pokemon {Id = 2, Name = "Oshawott", Count = 30, UserId = 1};
+
+        var result = controller.Update(2, updatedPokemon);
+        var pokemonResult = controller.Get(2);
+        var pokemon = pokemonResult.Value;
+
+        pokemon.Should().BeEquivalentTo(updatedPokemon);
+        updatedPokemon = new Pokemon {Id = 2, Name = "Oshawott", Count = 24, UserId = 1};
+        controller.Update(2, updatedPokemon);
     }
 }
